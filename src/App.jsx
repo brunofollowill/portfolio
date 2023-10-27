bf43553eed93daafa2fe1d28e5a7e15a08f1c32e
westerdeveloper <brunodev_@outlook.com>
2023-10-27 13:14:57 -0300

    atualizei

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/layout/header/Header";
 import "./Input.css";
 import Home from "./components/pages/home/Home";
-import About from "./components/pages/about/About";
+// import About from "./components/pages/about/About";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
     <Home />
-    <About />
+    {/* <About /> */}
   </React.StrictMode>
 );
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,21 +1,19 @@
-import React from "react";
-
 export default function Home() {
   return (
     <div>
       <div className="flex m-auto md:ml-60 ">
-        <div className=" mt-36 m-auto">
+        <div className=" max-sm:mt-20 mt-36 m-auto">
           <h1 className="font-poppins font-black sm:text-8xl text-6xl text-sky-500">
             Oi, eu sou Bruno.
           </h1>
-          <h3 className="text-black text-3xl sm:text-3xl mt-3 font-thin">
+          <h3 className="text-stone-600 text-3xl sm:text-3xl mt-1 font-thin">
             Sou um
             <span className="text-sky-500 font-poppins text font-black">
               {" "}
               desenvolvedor web
             </span>
           </h3>
-          <p className=" xl:w-2/3 sm:w-96 text-xl font-light mt-10">
+          <p className=" xl:w-2/3 sm:w-96 text-xl font-light mt-10 text-stone-600">
             Nascido e criado no Rio de Janeiro, há pouco mais de 1 ano embarquei
             no estudo do mundo da tecnologia. Sua constante evolução e
             possibilidades têm me cativado de forma crescente. Estou animado
