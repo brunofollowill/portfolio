52f24179c0bc4b7c1bc371e4a7ce0a12c9788f1b
brunofollowill <brunodev_@outlook.com>
2024-01-15 20:48:26 -0300

    atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import ScrollReveal from "scrollreveal";
 import Header from "./components/layout/header/Header";
 import "./Input.css";
 import Home from "./components/pages/home/Home";
@@ -7,16 +8,42 @@ import About from "./components/pages/about/About";
 import Footer from "./components/pages/footer/Footer";
 import Technologies from "./components/pages/technologies/Technologies";
 import Projects from "./components/pages/projects/Projects";
-import Contact  from "./components/pages/contact/Contact";
+import Contact from "./components/pages/contact/Contact";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Header />
-    <Home />
-    <About />
-    <Technologies />
-    <Projects />
-    <Contact />
-    <Footer />
-  </React.StrictMode>
-);
+const App = () => {
+  useEffect(() => {
+    // Inicializa o ScrollReveal
+    ScrollReveal().reveal(".scroll-reveal", {
+      duration: 1000,
+      easing: "ease-in-out",
+      scale: 1,
+      distance: "20px",
+      origin: "bottom",
+      reset: true,
+    });
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <div className="scroll-reveal">
+        <Home />
+      </div>
+      <div className="scroll-reveal">
+        <About />
+      </div>
+      <div className="scroll-reveal">
+        <Technologies />
+      </div>
+      <div className="scroll-reveal">
+        <Projects />
+      </div>
+      <div className="scroll-reveal">
+        <Contact />
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -27,14 +27,14 @@ export default function Home() {
         }}
       >
         <div
-          className="aspect-[1055/778] w-[77.1875rem] max-sm:w-[20.1875rem] bg-gradient-to-tr from-[#96cdfa] to-[#0c80ec] opacity-50 max-sm:opacity-100 "
+          className="aspect-[1055/778] w-[77.1875rem] max-sm:w-[20.1875rem] bg-gradient-to-tr from-[#96cdfa] to-[#0c80ec] opacity-40 max-sm:opacity-100 "
           style={{
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
         />
         <div
-          className="aspect-[1055/778] w-[77.1875rem] max-sm:mt-96 max-sm:w-[20.1875rem] bg-gradient-to-tr from-[#96cdfa] to-sky-500 opacity-50 max-sm:opacity-90 "
+          className="aspect-[1055/778] w-[77.1875rem] max-sm:mt-96 max-sm:w-[20.1875rem] bg-gradient-to-tr from-[#96cdfa] to-sky-500 opacity-30 max-sm:opacity-90 "
           style={{
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
